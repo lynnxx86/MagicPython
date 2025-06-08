a79f902e7e8c42607a99f328988ce05ab068373f
steviexx <hoviousstevie@gmail.com>
2025-06-08 18:56:57 -0500

    Initial commit for MagicPython

---
+++ Magic8_Win.py
@@ -0,0 +1,193 @@
+import webbrowser
+import random
+print("Welcome to Stevie's magic 8 program!")
+print("Named after the magic 8 ball toy of yesteryear, my program will answer any yes/no question you ask!")
+usr_nme = input("What's your name?" )
+
+char_count = 0
+for char in usr_nme:
+ char_count += 1
+
+question = input("What's your question?: ")
+
+random_num = random.randint(char_count)
+
+if (random_num == 1):
+ print("Outlook not so good!")
+ input("Press any key to close the program.")
+elif (random_num == 23):
+ print("Ask again later!")
+ input("Press any key to close the program.")
+elif (random_num == 3):
+ print("Better not tell you now!")
+ input("Press any key to close the program.")
+elif (random_num == 24):
+ print("Go for it!")
+ input("Press any key to close the program.")
+elif (random_num == 5):
+ print("The spirits are on your side today!")
+ input("Press any key to close the program.")
+elif (random_num == 6):
+ print("The spirits are not working in your favor on this endeavor.")
+ input("Press any key to close the program.")
+elif (random_num == 21):
+ print("How the fuck should I know? I'm a line of code.")
+ input("Press any key to close the program.")
+elif (random_num == 8):
+ print("Best of luck... you'll need it.")
+ input("Press any key to close the program.")
+elif (random_num == 9):
+ print("You're gonna kill it!")
+ input("Press any key to close the program.")
+elif (random_num == 20):
+ print("They aren't ready for someone like you!")
+ input("Press any key to close the program.")
+elif (random_num == 11):
+ print("Bring the THUNDA")
+ input("Press any key to close the program.")
+elif (random_num == 12):
+ print("Maybe you should stay home today.")
+ input("Press any key to close the program.")
+elif  (random_num == 13):
+ print("Do whatever you want, idk.")
+ input("Press any key to close the program.")
+elif (random_num == 14):
+ print("You already have everything you need for this adventure within. Go get it!")
+ input("Press any key to close the program.")
+elif (random_num == 22):
+ print("It is certain.")
+ input("Press any key to close the program.")
+elif (random_num == 16):
+ print("It is decidedly so.")
+ input("Press any key to close the program.")
+elif (random_num == 17):
+ print("Without a doubt")
+ input("Press any key to close the program.")
+elif (random_num == 18):
+ print("Yes, definetly.")
+ input("Press any key to close the program.")
+elif (random_num == 19):
+ print("You may rely on it")
+elif (random_num == 10):
+ print("As I see it, yes.")
+ input("Press any key to close the program.")
+elif (random_num == 7):
+ print("Most likely")
+ input("Press any key to close the program.")
+elif (random_num == 15):
+ print("Outlook good!")
+ input("Press any key to close the program.")
+elif (random_num == 2):
+ print("Yes")
+ input("Press any key to close the program.")
+elif (random_num == 4):
+ webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+elif (random_num == 25):
+ print("Try restarting the computer and asking again!")
+ input("Press any key to close the program.")
+elif (random_num == 26):
+ print("Don't do it.")
+ input("Press any key to close the program.")
+elif (random_num == 27):
+ print("Maybe you should stay home today.")
+ input("Press any key to close the program.")
+elif  (random_num == 28):
+ print("Do whatever you want, idk.")
+ input("Press any key to close the program.")
+elif (random_num == 29):
+ print("It's time to suck today's dick!!")
+ input("Press any key to close the program.")
+elif (random_num == 30):
+ print("I believe in you!")
+ input("Press any key to close the program.")
+elif (random_num == 31):
+ print("It has been decided without you.")
+ input("Press any key to close the program.")
+elif (random_num == 32):
+ print("Make haste!")
+ input("Press any key to close the program.")
+elif (random_num == 33):
+ print("Yes, you should.")
+ input("Press any key to close the program.")
+elif (random_num == 34):
+ print("You may.")
+ input("Press any key to close the program.")
+elif (random_num == 35):
+ print("Ehhhhh.. screw it")
+ input("Press any key to close the program.")
+elif (random_num == 36):
+ print("Yeah, totally!")
+ input("Press any key to close the program.")
+elif (random_num == 37):
+ print("I thknk it's a good idea!")
+ input("Press any key to close the program.")
+elif (random_num == 38):
+ print("Yap")
+ input("Press any key to close the program.")
+elif (random_num == 39):
+ print("Nah G")
+ input("Press any key to close the program.")
+elif (random_num == 40):
+ print("Try restarting the software and asking again!")
+ input("Press any key to close the program.")
+elif (random_num == 41):
+ print("Don't think twice, just go for it.")
+ input("Press any key to close the program.")
+elif (random_num == 42):
+ print("69")
+ input("Press any key to close the program.")
+elif  (random_num == 43):
+ print("Use protection if you do.")
+ input("Press any key to close the program.")
+elif (random_num == 44):
+ print("This does not sound like a good idea")
+ input("Press any key to close the program.")
+elif (random_num == 45):
+ print("I don't believe in you")
+ input("Press any key to close the program.")
+elif (random_num == 46):
+ print("Get some chicken strips first.")
+ input("Press any key to close the program.")
+elif (random_num == 47):
+ print("Absolutely not!")
+ input("Press any key to close the program.")
+elif (random_num == 48):
+ print("Yes, you should not.")
+ input("Press any key to close the program.")
+elif (random_num == 49):
+ print("You may not.")
+ input("Press any key to close the program.")
+elif (random_num == 50):
+ print("It seems dangerous...")
+ input("Press any key to close the program.")
+elif (random_num == 51):
+ print("Yeah, I don't know about that.")
+ input("Press any key to close the program.")
+elif (random_num == 52):
+ print("I don't like the idea")
+ input("Press any key to close the program.")
+elif (random_num == 53):
+ print("Yah")
+ input("Press any key to close the program.")
+elif (random_num == 54):
+ print("No way, Jose.")
+ input("Press any key to close the program.")
+elif (random_num == 55):
+ print("Try restarting the universe and asking again!")
+ input("Press any key to close the program.")
+elif (random_num == 56):
+ print("Pull the trigger!")
+ input("Press any key to close the program.")
+
+
+
+
+
+
+elif (question == "Should I kill myself?"):
+ print("No!")
+ input("Press any key to close the program.")
+else:
+ print("Error!")
+ input("Press any key to close the program.")
+
